1294d347035026d540fdd73f76b53a0b4008367d
RedOptic <CommanderBeaver@Gmail.com>
2024-08-16 19:27:56 -0400

    Added more items

--- src/main/java/net/redoptic/boozinmod/item/ModItems.java
+++ src/main/java/net/redoptic/boozinmod/item/ModItems.java
@@ -11,6 +11,24 @@ import net.redoptic.boozinmod.BoozinMod;
 public class ModItems {
     public static final Item YEAST = registerItem("yeast", new Item(new Item.Settings()));
     public static final Item MOLASSES = registerItem("molasses", new Item(new Item.Settings()));
+    public static final Item MASH = registerItem("mash", new Item(new Item.Settings()));
+    public static final Item WORT = registerItem("wort", new Item(new Item.Settings()));
+    public static final Item BEER_MUG_EMPTY = registerItem("beer_mug_empty", new Item(new Item.Settings()));
+    public static final Item BEER_MUG_FULL = registerItem("beer_mug_full", new Item(new Item.Settings()));
+    public static final Item MEAD_GLASS_EMPTY = registerItem("mead_glass_empty", new Item(new Item.Settings()));
+    public static final Item MEAD_GLASS_FULL = registerItem("mead_glass_full", new Item(new Item.Settings()));
+    public static final Item MOONSHINE_JAR_EMPTY = registerItem("moonshine_jar_empty", new Item(new Item.Settings()));
+    public static final Item MOONSHINE_JAR_FULL = registerItem("moonshine_jar_full", new Item(new Item.Settings()));
+    public static final Item RUM_GLASS_EMPTY = registerItem("rum_glass_empty", new Item(new Item.Settings()));
+    public static final Item RUM_GLASS_FULL = registerItem("rum_glass_full", new Item(new Item.Settings()));
+    public static final Item RUM_GLASS_UNAGED_FULL = registerItem("rum_glass_unaged_full", new Item(new Item.Settings()));
+    public static final Item WHISKEY_GLASS_EMPTY = registerItem("whiskey_glass_empty", new Item(new Item.Settings()));
+    public static final Item WHISKEY_GLASS_FULL = registerItem("whiskey_glass_full", new Item(new Item.Settings()));
+    public static final Item WINE_GLASS_EMPTY = registerItem("wine_glass_empty", new Item(new Item.Settings()));
+    public static final Item WINE_GLASS_FULL = registerItem("wine_glass_full", new Item(new Item.Settings()));
+    public static final Item UNDISTILLED_RUM = registerItem("undistilled_rum", new Item(new Item.Settings()));
+    public static final Item UNDISTILLED_WHISKEY = registerItem("undistilled_whiskey", new Item(new Item.Settings()));
+
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(BoozinMod.MOD_ID, name), item);
@@ -20,8 +38,6 @@ public class ModItems {
         BoozinMod.LOGGER.info("Registering Mod Items for " + BoozinMod.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(YEAST);
-            entries.add(MOLASSES);
         });
     }
 }
