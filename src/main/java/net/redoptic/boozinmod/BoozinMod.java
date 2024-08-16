1962db7529bd40c59147808a4a799ca98dd23897
RedOptic <CommanderBeaver@Gmail.com>
2024-08-16 17:33:52 -0400

    Changed yeast texture

--- src/main/java/net/redoptic/boozinmod/BoozinMod.java
+++ src/main/java/net/redoptic/boozinmod/BoozinMod.java
@@ -2,15 +2,16 @@ package net.redoptic.boozinmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.redoptic.boozinmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BoozinMod implements ModInitializer {
-	public static final String MOD_ID = "BoozinMod";
+	public static final String MOD_ID = "boozinmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
